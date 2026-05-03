5db6a759fe5514a55a05e3d968e162892c85ec6a
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-03 18:21:14 +0100

    Added arithmatic operators for the 3D vector class

--- Introduction_to_Ray_Tracing/raytrace.cpp
+++ Introduction_to_Ray_Tracing/raytrace.cpp
@@ -15,7 +15,23 @@ class Vec3
         Vec3(T xx) : x(xx), y(xx), z(xx) {}
         Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
 
-}
+        Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
+        Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
+        Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
+        Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
+        Vec3<T> operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
+        Vec3<T> operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+        Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
+
+        friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
+        {
+            os << "[" << v.x << " " << v.y << " " << v.z << "]";
+            return os;
+        }
+
+        
+
+};
 
 int main(int argc, char **argv)
 {
