0837f6c3887dae40d2c9c69772e40b8ef1b3955a
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-03 18:08:53 +0100

    Added class for 3D vectors with constructors

--- Introduction_to_Ray_Tracing/raytrace.cpp
+++ Introduction_to_Ray_Tracing/raytrace.cpp
@@ -6,6 +6,17 @@
 #include <iostream>
 #include <cassert>
 
+template<typename T>
+class Vec3
+{
+    public:
+        T x, y, z;
+        Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+        Vec3(T xx) : x(xx), y(xx), z(xx) {}
+        Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+
+}
+
 int main(int argc, char **argv)
 {
     return 0;
