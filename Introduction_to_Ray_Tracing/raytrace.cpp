5763f10d0a36e4d13c49e955f19745f81d630b80
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-04 21:01:58 +0100

    Added render function to create the image and create an eye ray for each pixel (This segfaults)

--- Introduction_to_Ray_Tracing/raytrace.cpp
+++ Introduction_to_Ray_Tracing/raytrace.cpp
@@ -79,6 +79,7 @@ class Sphere
 };
 
 #define MAX_RAY_DEPTH 5
+#define DEFAULT_FOV 30.
 
 float mix(const float &a, const float &b, const float &mix)
 {
@@ -164,15 +165,51 @@ Vec3f trace(const Vec3f &ray_origin, const Vec3f &ray_dir, const std::vector<Sph
     return surface_color + sphere->emission_color;
 }
 
+void render(const std::vector<Sphere> &spheres)
+{
+    unsigned width = 700, height = 500;
+    Vec3f *image = new Vec3f[width * height], *pixel = image;
+    float width_inv = 1 / float(width), height_inv = 1 / float(height);
+    float aspect_ratio = width / float(height);
+    float angle = tan(M_PI * 0.5 * DEFAULT_FOV / 100.);
+
+    for (unsigned x = 0; x < width; x++)
+    {
+        for (unsigned y = 0; y < height; y++)
+        {
+            float xx = (2 * ((x + 0.5) * width_inv) - 1) * angle * aspect_ratio;
+            float yy = (1 - 2 * ((y + 0.5) * height_inv)) * angle;
+            Vec3f ray_dir(xx, yy, -1);
+            ray_dir.normalize();
+            *pixel = trace(Vec3f(0), ray_dir, spheres, 0);
+        }
+    }
+
+    std::ofstream ofs("./raytrace.ppm", std::ios::out | std::ios::binary);
+    ofs << "P6\n" << width << " " << height << "\n255\n";
+    for (unsigned i = 0; i < width * height; i++)
+    {
+        ofs << (unsigned char) (std::min(float(1), image[i].x) * 255) <<
+                (unsigned char) (std::min(float(1), image[i].y) * 255) <<
+                (unsigned char) (std::min(float(1), image[i].z) * 255);
+    }
+    ofs.close();
+    delete [] image;
+}
+
 int main(int argc, char **argv)
 {
-    srand48(42);
+    srand48(16);
     std::vector<Sphere> spheres;
-    spheres.push_back(Sphere(Vec3f( 0.0, ), 0.0, Vec3f( 0.0, ), 0.0, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0,      0, -20),     4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
+    spheres.push_back(Sphere(Vec3f( 5.0,     -1, -15),     2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
+    spheres.push_back(Sphere(Vec3f( 5.0,      0, -25),     3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
+    spheres.push_back(Sphere(Vec3f(-5.5,      0, -15),     3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
 
     //Light
     spheres.push_back(Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
-    //render(spheres);
+    render(spheres);
 
     return 0;
 }
