b66e5bc28e905d39626cb904b210457b5a408183
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-04 16:32:21 +0100

    Added the main logic of the renderer, a recursive trace function to trace the path eye rays through the scene

--- Introduction_to_Ray_Tracing/raytrace.cpp
+++ Introduction_to_Ray_Tracing/raytrace.cpp
@@ -61,7 +61,7 @@ class Sphere
             const float &refl = 0,
             const float &transp = 0,
             const Vec3f &ec = 0
-        ) : center(c), radius(r), radius_sqr(r * r), surface_color(sc), emission_color(ec), transparency(transp), reflection(refl)
+        ) : center(c), radius(r), radius_sqr(r * r), surface_color(sc), emission_color(ec), transparency(transp), reflection(refl) {}
 
         bool intersect(const Vec3f &ray_origin, const Vec3f &ray_dir, float &t0, float &t1) const
         {
@@ -79,6 +79,90 @@ class Sphere
 };
 
 #define MAX_RAY_DEPTH 5
+
+float mix(const float &a, const float &b, const float &mix)
+{
+    return b * mix + a * (1 - mix);
+}
+
+Vec3f trace(const Vec3f &ray_origin, const Vec3f &ray_dir, const std::vector<Sphere> &spheres, const int &depth)
+{
+    float tnear = INFINITY;
+    const Sphere* sphere = NULL;
+
+    for (unsigned i = 0; i < spheres.size(); i++)
+    {
+        float t0 = INFINITY, t1 = INFINITY;
+        if (spheres[i].intersect(ray_origin, ray_dir, t0, t1))
+        {
+            if (t0 < 0) t0 = t1;
+            if (t0 < tnear)
+            {
+                tnear = t0;
+                sphere = &spheres[i];
+            }
+        }
+    }
+
+    if (!sphere) return Vec3f(2);
+    Vec3f surface_color = 0;
+    Vec3f phit = ray_origin + ray_dir * tnear;
+    Vec3f nhit = phit - sphere->center;
+    nhit.normalize();
+
+    float bias = 1e-4;
+    bool inside = false;
+    if (ray_dir.dot(nhit) > 0) nhit = -nhit, inside = true;
+    if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH)
+    {
+        float facing_ratio = -ray_dir.dot(nhit);
+        float fresnel_effect = mix(pow(1 - facing_ratio, 3), 1, 0.1);
+        Vec3f refl_dir = ray_dir - nhit * 2 * ray_dir.dot(nhit);
+        refl_dir.normalize();
+        Vec3f reflection = trace(phit + nhit * bias, refl_dir, spheres, depth + 1);
+        Vec3f refraction = 0;
+
+        if (sphere->transparency)
+        {
+            float ior = 1.1, eta = (inside) ? ior : 1 / ior;
+            float cosi = -nhit.dot(ray_dir);
+            float k = 1 - eta * eta * (1 - cosi * cosi);
+            Vec3f refr_dir = ray_dir * eta + nhit * (eta * cosi - sqrt(k));
+            refr_dir.normalize();
+            refraction = trace(phit - nhit * bias, refr_dir, spheres, depth + 1);
+        }
+
+        surface_color = (reflection * fresnel_effect + refraction * (1 - fresnel_effect) * sphere->transparency) * sphere->surface_color;
+    }
+
+    else
+    {
+        for (unsigned i = 0; i < spheres.size(); i++)
+        {
+            if (spheres[i].emission_color.x > 0)
+            {
+                Vec3f transmission = 1;
+                Vec3f light_dir = spheres[i].center - phit;
+                light_dir.normalize();
+                for (unsigned j = 0; i < spheres.size(); j++)
+                {
+                    if (i != j)
+                    {
+                        float t0, t1;
+                        if (spheres[j].intersect(phit + nhit * bias, light_dir, t0, t1))
+                        {
+                            transmission = 0;
+                            break;
+                        }
+                    }
+                }
+                surface_color += sphere->surface_color * transmission * std::max(float(0), nhit.dot(light_dir)) * spheres[i].emission_color;
+            }
+        }
+    }
+
+    return surface_color + sphere->emission_color;
+}
 
 int main(int argc, char **argv)
 {
