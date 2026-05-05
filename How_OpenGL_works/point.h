ad9489c8943a9c5f43eadc775e1d642ea1cce0b7
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-05 16:05:19 +0100

    Added header struct to represent a vertex (to be expanded)

---
+++ How_OpenGL_works/point.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cassert.h>
+
+template<int n> struct Point
+{
+    double data[n] = {0};
+    double& operator[](const int i)       { assert(i>=0 && i<n); return data[i]; }
+    double  operator[](const int i) const { assert(i>=0 && i<n); return data[i]; }
+};
+
+template<> struct Point<2>
+{
+    double x = 0, y = 0;
+    double& operator[](const int i)       { assert(i>=0 && i<2); return i ? y : x; }
+    double  operator[](const int i) const { assert(i>=0 && i<2); return i ? y : x; }
+};
+
+template<> struct Point<3>
+{
+    double x = 0, y = 0, z = 0;
+    double& operator[](const int i)       { assert(i>=0 && i<3); return i ? (1==i ? y : z) : x; }
+    double  operator[](const int i) const { assert(i>=0 && i<3); return i ? (1==i ? y : z) : x; }
+};
+
+typedef Point<2> Point2;
+typedef Point<3> Point3;
