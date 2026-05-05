abab2476dc84e7e58e94ba760803719b3e75844f
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-05 16:56:29 +0100

    Updated triangle function to fill in the triangles using scanline rasterisation

--- How_OpenGL_works/main.cpp
+++ How_OpenGL_works/main.cpp
@@ -46,9 +46,36 @@ void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
 
 void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color)
 {
-    line(ax, ay, bx, by, framebuffer, color);
-    line(bx, by, cx, cy, framebuffer, color);
-    line(cx, cy, ax, ay, framebuffer, color);
+    // Bubblesort to sort verticies by y coordinate
+    if (ay > by) { std::swap(ax, bx); std::swap(ay, by); }
+    if (ay > cy) { std::swap(ax, cx); std::swap(ay, cy); }
+    if (by > cy) { std::swap(bx, cx); std::swap(by, cy); }
+
+    int total_height = cy - ay;
+
+    if (ay != by)
+    {
+        int segment_height = by - ay;
+        for (int y = ay; y <= by; y++)
+        {
+            int x1 = ax + ((cx - ax) * (y - ay)) / total_height;
+            int x2 = ax + ((bx - ax) * (y - ay)) / segment_height;
+            for (int x = std::min(x1, x2); x < std::max(x1, x2); x++)
+                framebuffer.set(x, y, color);
+        }
+    }
+
+    if (by != cy)
+    {
+        int segment_height = cy - by;
+        for (int y = by; y <= cy; y++)
+        {
+            int x1 = ax + ((cx - ax) * (y - ay)) / total_height;
+            int x2 = bx + ((cx - bx) * (y - by)) / segment_height;
+            for (int x = std::min(x1, x2); x < std::max(x1, x2); x++)
+                framebuffer.set(x, y, color);
+        }
+    }
 }
 
 int main(int argc, char **argv)
