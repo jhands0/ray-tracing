982652893b8f472cb77c79930d67e0e25cbb3f8b
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-05 14:33:29 +0100

    Added line drawing function using Bresenham's line algorithm

--- How_OpenGL_works/main.cpp
+++ How_OpenGL_works/main.cpp
@@ -11,6 +11,34 @@ constexpr TGAColor red =    {0, 0, 255, 255};
 constexpr TGAColor blue =   {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
+{
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep)
+    {
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+
+    if (ax > bx)
+    {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+
+    for (int x = ax; x <= bx; x++)
+    {
+        float t = (x - ax) / static_cast<float>(bx - ax);
+        int y = std::round(ay + (by - ay) * t);
+        
+        if (steep) 
+            framebuffer.set(y, x, color);
+        
+        else 
+            framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char **argv)
 {
     constexpr int width = 64;
@@ -20,6 +48,11 @@ int main(int argc, char **argv)
     int ax = 7, ay = 3;
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
