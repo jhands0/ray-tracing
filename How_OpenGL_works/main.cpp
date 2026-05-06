668fc90bb94de490d7f25622a5c8abbe4e38c4ee
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-06 14:39:04 +0100

    Added model rendering into the main program

--- How_OpenGL_works/main.cpp
+++ How_OpenGL_works/main.cpp
@@ -3,11 +3,13 @@
 // cmake --build ./build
 
 #include <cmath>
+#include <tuple>
+#include "vec.h"
 #include "model.h"
 #include "tgaimage.h"
 
-constexpr int width = 128;
-constexpr int height = 128;
+constexpr int width = 500;
+constexpr int height = 500;
 
 constexpr TGAColor white =  {255, 255, 255, 255};
 constexpr TGAColor green =  {0, 255, 0, 255};
@@ -50,13 +52,14 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy)
     return .5 * ((by - ay)*(bx + ax) + (cy - by)*(cx + bx) + (ay - cy)*(ax + cx));
 }
 
-void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &framebuffer)
+void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &framebuffer, TGAColor color)
 {
     int box_min_x = std::min(std::min(ax, bx), cx);
     int box_min_y = std::min(std::min(ay, by), cy);
     int box_max_x = std::max(std::max(ax, bx), cx);
     int box_max_y = std::max(std::max(ay, by), cy);
     double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+    if (total_area < 1) return;
     
 #pragma omp parallel for
     for (int x = box_min_x; x <= box_max_x; x++)
@@ -67,28 +70,59 @@ void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, in
             double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha < 0 || beta < 0 || gamma < 0) continue;
-            float max = std::max(std::max(alpha + beta, beta + gamma), alpha + gamma);
-            if (max > 0.90f)
-            {
-                uint8_t red = gamma * 255;
-                uint8_t green = beta * 255;
-                uint8_t blue = alpha * 255;
-                TGAColor color = {blue, green, red};
-                framebuffer.set(x, y, color);
-            }
+            
+            // Lines to create wireframes
+            //float max = std::max(std::max(alpha + beta, beta + gamma), alpha + gamma);
+            //if (max > 0.90f) {}
+
+            // Lines for rainbow triangles
+            //uint8_t red = gamma * 255;
+            //uint8_t green = beta * 255;
+            //uint8_t blue = alpha * 255;
+            //TGAColor color = {blue, green, red};
+
+            framebuffer.set(x, y, color);
         }
     }
+}
+
+// Scale model to dimensions of output file
+std::tuple<int, int> project(Vec3 vert)
+{
+    return { (vert.x + 1.) * width / 2, (vert.y + 1.) * height / 2 };
 }
 
 int main(int argc, char **argv)
 {
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax = 17, ay = 4, az = 13;
-    int bx = 55, by = 39, bz = 128;
-    int cx = 23, cy = 59, cz = 255;
+    if (argc < 2)
+    {
+        int ax = 17, ay = 4, az = 13;
+        int bx = 55, by = 39, bz = 128;
+        int cx = 23, cy = 59, cz = 255;
 
-    triangle(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer);
+        triangle(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer, red);
+    }
+    else if (argc == 2)
+    {
+        Model model = Model(argv[1]);
+
+        for (int i = 0; i < model.n_faces(); i++)
+        {
+            auto [ax, ay] = project(model.vert(i, 0));
+            auto [bx, by] = project(model.vert(i, 1));
+            auto [cx, cy] = project(model.vert(i, 2));
+            TGAColor color;
+            for (int c = 0; c < 3; c++) color[c] = std::rand() % 255;
+            triangle(ax, ay, 0, bx, by, 0, cx, cy, 0, framebuffer, color);
+        }
+    }
+    else 
+    {
+        std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+        return 1;
+    }
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
