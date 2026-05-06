0893240edbf6d6d1d8c416c4bf5a9eba249906b4
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-06 09:58:27 +0100

    Added az, bz, and cz parameters to add shading to the triangle

--- How_OpenGL_works/main.cpp
+++ How_OpenGL_works/main.cpp
@@ -49,14 +49,14 @@ double signed_triangle_area(int ax, int ay, int bx, int by, int cx, int cy)
     return .5 * ((by - ay)*(bx + ax) + (cy - by)*(cx + bx) + (ay - cy)*(ax + cx));
 }
 
-void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color)
+void triangle(int ax, int ay, int az, int bx, int by, int bz, int cx, int cy, int cz, TGAImage &framebuffer)
 {
     int box_min_x = std::min(std::min(ax, bx), cx);
     int box_min_y = std::min(std::min(ay, by), cy);
     int box_max_x = std::max(std::max(ax, bx), cx);
     int box_max_y = std::max(std::max(ay, by), cy);
     double total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
-
+    
 #pragma omp parallel for
     for (int x = box_min_x; x <= box_max_x; x++)
     {
@@ -66,18 +66,22 @@ void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuf
             double beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
             double gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
             if (alpha < 0 || beta < 0 || gamma < 0) continue;
-            framebuffer.set(x, y, color);
+            unsigned char z = static_cast<unsigned char>(alpha * az + beta * bz + gamma * cz);
+            if (std::min(alpha, std::min(beta, gamma)) > .1) 
+                framebuffer.set(x, y, {z});
         }
     }
 }
 
 int main(int argc, char **argv)
 {
-    TGAImage framebuffer(width, height, TGAImage::RGB);
+    TGAImage framebuffer(width, height, TGAImage::GRAYSCALE);
 
-    triangle(7, 45, 35, 100, 45, 60, framebuffer, red);
-    triangle(120, 35, 90, 5, 45, 110, framebuffer, white);
-    triangle(115, 83, 80, 90, 85, 120, framebuffer, green);
+    int ax = 17, ay = 4, az = 13;
+    int bx = 55, by = 39, bz = 128;
+    int cx = 23, cy = 59, cz = 255;
+
+    triangle(ax, ay, az, bx, by, bz, cx, cy, cz, framebuffer);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
