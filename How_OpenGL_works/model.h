363bea4dcc9394083d819cb70645219a3f128940
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-06 13:41:23 +0100

    Added model class to represent .obj files

---
+++ How_OpenGL_works/model.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include "vec.h"
+
+class Model 
+{
+    std::vector<Vec3> verts = {};
+    std::vector<int> faces = {};
+public:
+    Model(const std::string filename);
+    int n_verts() const;
+    int n_faces() const;
+    Vec3 vert(const int idx) const;
+    Vec3 vert(const int face_i, const int nth_vert) const;
+}
