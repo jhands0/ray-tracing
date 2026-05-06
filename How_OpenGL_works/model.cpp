2d020977f4d5c516afcd637944a0b1b1500c0a4e
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-06 15:16:20 +0100

    Added the painter's algorithm to sort triangles by depth in the model class

--- How_OpenGL_works/model.cpp
+++ How_OpenGL_works/model.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <sstream>
 #include "model.h"
+#include <algorithm>
 
 Model::Model(const std::string filename)
 {
@@ -37,11 +38,35 @@ Model::Model(const std::string filename)
         }
     }
     std::cerr << "# v# " << n_verts() << " f# " << n_faces() << std::endl;
+
+    std::vector<int> idx(n_faces());
+    for (int i = 0; i < n_faces(); i++) idx[i] = i;
+
+    std::sort(idx.begin(), idx.end(),
+            [&](const int& a, const int& b) { // given two triangles, compare their min z coordinate
+                  float aminz = std::min(vert(a, 0).z,
+                                         std::min(vert(a, 1).z,
+                                                  vert(a, 2).z));
+                  float bminz = std::min(vert(b, 0).z,
+                                         std::min(vert(b, 1).z,
+                                                  vert(b, 2).z));
+                  return aminz < bminz;
+            });
+    
+    std::vector<int> faces_2(n_faces() * 3);
+    for (int i = 0; i < n_faces(); i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            faces_2[i * 3 + j] = faces[idx[i] * 3 + j];
+        }
+    }
+    faces = faces_2;
 }
 
 int Model::n_verts() const { return verts.size(); }
-int Model::n_faces() const { return faces.size(); }
+int Model::n_faces() const { return faces.size() / 3; }
 
 Vec3 Model::vert(const int i) const { return verts[i]; }
 
-Vec3 Model::vert(const int face_i, const int nth_vert) const { return verts[faces[face_i + nth_vert]]; }
+Vec3 Model::vert(const int face_i, const int nth_vert) const { return verts[faces[face_i * 3 + nth_vert]]; }
