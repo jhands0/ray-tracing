af3dbbc094c46c5d8ba000f4317164ddd671c299
Jamie Hands <jamie_hands0@protonmail.com>
2026-05-11 19:04:42 +0100

    Added to represent a matrix

--- How_OpenGL_works/vec.h
+++ How_OpenGL_works/vec.h
@@ -96,3 +96,13 @@ inline Vec3 cross(const Vec3 &v1, const Vec3 &v2)
 {
     return {v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x};
 }
+
+template<int n> struct dt;
+
+template<int n_rows, int n_cols> struct matrix
+{
+    Vec<n_cols> rows[n_rows] = {{}};
+
+        Vec<n_cols>& operator [] (const int idx)    { assert(idx >= 0 && idx < n_rows); return rows[idx]; }
+    const Vec<n_cols>& operator [] (const int idx)  { assert(idx >= 0 && idx < n_rows); return rows[idx]; }
+}
